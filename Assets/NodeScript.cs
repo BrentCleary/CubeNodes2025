0f8c5dca46cac42d6ee0979ffe66ff35d4403639
Brent Cleary <brent.cleary@gmail.com>
2024-07-13 17:36:45 -0700

    Commit With Cube Stack

--- Assets/NodeScript.cs
+++ Assets/NodeScript.cs
@@ -6,35 +6,19 @@ using UnityEngine;
 public class NodeScript : MonoBehaviour
 {
 
-    // NODE VARIABLES
-    List<int> nodeValue = new List<int> { 0, 1, 2, 3, 4 };   // Node Values when not occupied
-    List<bool> placeable = new List<bool> { false, true };   // is node placeable for current player
-    List<int> libertyValue = new List<int> { 0, 1 }          //  LibertyValue{ 1 , 0 }
+    // NODE STATE VARIABLES
+    private List<int> nodeValue = new List<int> { 0, 1, 2, 3, 4 };   // Node Values when not occupied
+    private List<int> libertyValue = new List<int> { 0, 1 };          //  LibertyValue{ 1 , 0 }
+    private List<bool> placeable = new List<bool> { false, true };   // is node placeable for current player
     
-    // 
-    List<string> nodeContains = new List<string> { empty, sheepBlack, sheepWhite }; // List displaying current Node Occupant
+    public List<GameObject> GrassTileList = new List<GameObject> {};
     
-    // STATES
-    List<string> transitionStates = new List<string> { beingCaptured };             // States for transition
+    private List<string> nodeContains = new List<string> { "empty", "sheepBlack", "sheepWhite" }; // List displaying current Node GameObject
+    private List<string> transitionStates = new List<string> { "beingCaptured" };             // States for transition
 
 
-// OBJECT PROPERTIES
-// Objects for displaying value
-// - List containing game object references to display or toggle based on Node state
-// - To be hidden toggle their renders on or off
-
-
-// SHEEP OBJECT
-// Will need an over arching variable to determine behavior based on Black or White
-
-
-
-
-
-
-
-// Start is called before the first frame update
-void Start()
+    // Start is called before the first frame update
+    void Start()
     {
         
     }
