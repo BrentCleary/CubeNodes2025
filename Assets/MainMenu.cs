162bf43871de13dced0622e8c2bb4194995560af
Brent Cleary <brent.cleary@gmail.com>
2024-12-01 01:03:58 -0800

    Menu Creation

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -8,8 +8,15 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
+        SceneManager.LoadScene("9x9BoardScene");
 
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void QuitGame()
+    {
+        Debug.Log("Quit Button Clicked");
+        Application.Quit();
+    }
 
 }
