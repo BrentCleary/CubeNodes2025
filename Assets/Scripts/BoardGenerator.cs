05e03270347883919be7ea59d00e41ddb0cf5f26
Brent Cleary <brent.cleary@gmail.com>
2025-03-12 20:05:40 -0700

    Second Commit Test

--- Assets/Scripts/BoardGenerator.cs
+++ Assets/Scripts/BoardGenerator.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using Unity.PlasticSCM.Editor.WebApi;
-using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Analytics;
--- Assets/Scripts/NodeScript.cs
+++ Assets/Scripts/NodeScript.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using JetBrains.Annotations;
-using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Tilemaps;
